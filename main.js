f41d8b78ccca5229cd769ad7cb79ec8336ed7b8e
powch <cpcpowell8@gmail.com>
2018-04-21 16:40:10 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+//initial fade in for .post
+var fade = function() {
+    $(".post").hide().fadeIn(1000);
+}
+
+//discord bot link button
+var discordBtn = function() {
+    document.getElementById("discord").addEventListener("click", function() {
+        window.open("#");
+    });
+}
+
+//loads functions when DOM is loaded
+$(function() {
+   fade();
+   discordBtn();
+});
