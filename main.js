0e84c784ac92f876e800ef2b6999d80bb6cabc4e
powch <cpcpowell8@gmail.com>
2018-04-22 15:59:26 -0400

    workin on dropdowns

--- main.js
+++ main.js
@@ -10,26 +10,6 @@ var discordBtn = function() {
     });
 }
 
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function showToggle() {
-    document.getElementById("myDrop").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function (event) {
-    if (!event.target.matches('.dropdown-toggle')) {
-
-        var dropdowns = document.getElementsByClassName("dropdown-menu");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
 
 //loads functions when DOM is loaded
 $(function() {
