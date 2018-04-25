fe13367121964d179744fade037b1a5ca325584e
Chace Powell <cpcpowell8@gmail.com>
2018-04-24 20:42:02 -0400

    added video page and iframe embed. Added team page skeleton.

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 //initial fade in for .post
 var fade = function() {
-    $(".post, .quote").hide().fadeIn(1000);
+    $(".post, .quote").hide(0).fadeIn(1000);
 }
 
 //discord bot link button
-var discordBtn = function() {
-    document.getElementById("discord").addEventListener("click", function() {
-        window.open("#");
-    });
-}
+// var discordBtn = function() {
+//     document.getElementById("discord").addEventListener("click", function() {
+//         window.open("https://discordapp.com/oauth2/authorize?client_id=437363717076811808&permissions=19456&scope=bot");
+//     });
+// }
 
 //loads functions when DOM is loaded
 $(function() {
